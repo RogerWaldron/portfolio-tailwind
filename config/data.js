6a8179625178f7810946bbfc4f3e2586ab3c27fe
Roger Waldron <roger.waldron@gmail.com>
2022-10-24 23:25:29 +0300

    chore: Added Config folder and data.js

---
+++ config/data.js
@@ -0,0 +1,38 @@
+import { FaGithub, FaYoutube } from 'react-icons/fa'
+
+export const navData = [
+  {
+    page: 'home',
+  },
+  {
+    page: 'projects',
+  },
+  {
+    page: 'contact',
+  },
+]
+
+export const projectData = [
+  {
+    image: "",
+    title: "",
+    desc: "",
+    techs: [
+      {
+        icon: "",
+        name: "",
+      },
+    ]
+  }
+]
+
+export const socialMediaData = [
+  {
+    icon: <FaYoutube />,
+    url: '#',
+  },
+  {
+    icon: <FaGithub />,
+    url: '#',
+  },
+]
